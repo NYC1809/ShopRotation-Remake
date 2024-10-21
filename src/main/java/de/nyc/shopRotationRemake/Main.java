d7ec72e9301d20418077fbb2d13a06a92ef4b2f4
NYC_1809 <peterengelhardt1809@gmail.com>
2024-10-21 04:04:16 +0200

    initial Commit

--- src/main/java/de/nyc/shopRotationRemake/Main.java
+++ src/main/java/de/nyc/shopRotationRemake/Main.java
@@ -2,16 +2,19 @@ package de.nyc.shopRotationRemake;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        LocalDateTime start = LocalDateTime.now();
+        getLogger().info("loaded in " + Duration.between(start, LocalDateTime.now()).toMillis() + "ms");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }

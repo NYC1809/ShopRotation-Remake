38807c4482015d212258d1f7c2c9113efff5685b
NYC_1809 <peterengelhardt1809@gmail.com>
2024-11-03 23:16:35 +0100

    Added a few functions to get the values of the itemString

---
+++ src/main/java/de/nyc/shopRotationRemake/objects/Enchantments.java
@@ -0,0 +1,54 @@
+package de.nyc.shopRotationRemake.objects;
+
+import org.bukkit.enchantments.Enchantment;
+
+public enum Enchantments {
+
+    /*
+    PROTECTION("protection"),
+    FIRE_PROTECTION("fire_protection"),
+    FEATHER_FALLING("feather_falling"),
+    BLAST_PROTECTION("blast_protection"),
+    PROJECTILE_PROTECTION("projectile_protection"),
+    RESPIRATION("respiration"),
+    AQUA_AFFINITY("aqua_affinity"),
+    THORNS("thorns"),
+    DEPTH_STRIDER("depth_strider"),
+    FROST_WALKER("frost_walker"),
+    BINDING_CURSE("binding_curse"),
+    SHARPNESS("sharpness"),
+    SMITE("smite"),
+    BANE_OF_ARTHROPODS("bane_of_arthropods"),
+    KNOCKBACK("knockback"),
+    FIRE_ASPECT("fire_aspect"),
+    LOOTING("looting"),
+    SWEEPING_EDGE("sweeping_edge"),
+    EFFICIENCY("efficiency"),
+    SILK_TOUCH("silk_touch"),
+    UNBREAKING("unbreaking"),
+    FORTUNE("fortune"),
+    POWER("power"),
+    PUNCH("punch"),
+    FLAME("flame"),
+    INFINITY("infinity"),
+    LUCK_OF_THE_SEA("luck_of_the_sea"),
+    LURE("lure"),
+    LOYALTY("loyalty"),
+    IMPALING("impaling"),
+    RIPTIDE("riptide"),
+    CHANNELING("channeling"),
+    MULTISHOT("multishot"),
+    QUICK_CHARGE("quick_charge"),
+    PIERCING("piercing"),
+    DENSITY("density"),
+    BREACH("breach"),
+    WIND_BURST("wind_burst"),
+    MENDING("mending"),
+    VANISHING_CURSE("vanishing_curse"),
+    SOUL_SPEED("soul_speed"),
+    SWIFT_SNEAK("swift_sneak");
+
+*/
+
+
+}
